a21b9a00c7bade7446e9cc6d472c3e5b37427fc9
JoaquinHartwig <yanelian29@gmail.com>
2026-06-10 16:36:38 -0300

    corregi

---
+++ Aprendizaje/Propiedades de autoimplementacion/Propiedades de autoimplementacion/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Propiedades_de_autoimplementacion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
