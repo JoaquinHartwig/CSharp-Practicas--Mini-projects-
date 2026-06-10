04443a3e450d83d2daabd25974276b44c8d77ceb
JoaquinHartwig <yanelian29@gmail.com>
2026-06-10 02:13:44 -0300

    Interfaces (favorito)

---
+++ Aprendizaje/Interfaces/Interfaces/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Interfaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)//Una interfaz es parecida una clase padre...
+        {
+          conejo conejo = new conejo();//La ventaja q podes heredar mas de una interfaz
+            conejo.Huir();
+           Halcon halcon = new Halcon();
+            halcon.Atacar();
+            Pez pez = new Pez();
+            pez .Atacar();
+            pez.Huir();
+        }
+    }
+    interface IPresa //Siemrpe se pone una I para identificar mas facilmente
+    {
+        void Huir();//La implementacion es la responabilida de la clase q le hereda
+        
+    }
+    interface IDepredador 
+    {
+        void Atacar();
+    }
+    class conejo : IPresa
+    {
+        public void Huir()
+        {
+            Console.WriteLine("El conejo huye");
+        }
+
+    }
+    class Halcon : IDepredador 
+    {
+        public void Atacar()
+        {
+            Console.WriteLine("El halcon esta cazando");
+        }
+
+    }
+    class Pez : IPresa, IDepredador
+    {
+        public void Huir()
+        {
+            Console.WriteLine("El pez esta escapando");
+
+        }
+        public void Atacar()
+        {
+            Console.WriteLine("El pez esta buscando otro mas pequeño");
+        }
+    }
+}
